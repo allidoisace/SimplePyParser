c14620672eb29f9c8baf49a5d506abe378a45ecc
David Arce <arroservices@Live.com>
2017-05-01 00:33:33 -0700

    Update to a3.py and in.txt

--- a3.py
+++ a3.py
@@ -10,6 +10,12 @@ import re
 inputFile = open('in.txt', 'r')
 outputFile = open('out.txt', 'w')
 
+final = []
+
+def isOp(token):
+	# could just use else if's or dict or regex to test for op.
+	return True
+
 def isChar(token):
 	# use RegEx here for chars and return True else False
 	return True
@@ -20,16 +26,67 @@ def checkId(token):
 	else:
 		return False
 
+def isExpression(tokens):
+	if tokens[1] == ';':
+		final.append(tokens.pop(0)) # id
+		print(tokens)
+		return False
+	elif checkId(tokens[0]):
+		if isOp(tokens[1]):
+			print(tokens)
+			if checkId(tokens[2]):
+				if len(tokens) > 3:
+					if isOp(tokens[3]):
+						final.append(tokens.pop(0)) # id
+						final.append(tokens.pop(0)) # op
+						isExpression(tokens)
+					else:
+						print("Something WRONG!")
+					return True
+				else: 
+					return True
+			else:
+				return False
+		else:
+			return False
+	else:
+		return False
+		
 
+'''
+	If the statement is an 'Assignment', it will check for Id and '=',
+	then will pop off tokens and append to new list "Final". Then, while
+	there are still tokens, it will check for expression and add them to 
+	new list "Final" if they are 'id op id' format.
+'''
 def checkAssignment(tokens):
 	# will need to check if '(' or ')' are in tokens
 	if checkId(tokens[0][0]):
 		if '=' in tokens:
 			print("This is an assignment.")
+			final.append(tokens.pop(0)) # id
+			final.append(tokens.pop(0)) # '='
+			while len(tokens) != 0:
+				if tokens[0] == ';':
+					final.append(tokens.pop(0)) # ';'
+				elif isExpression(tokens):
+					final.append(tokens.pop(0)) # id
+					final.append(tokens.pop(0)) # op
+					final.append(tokens.pop(0)) # id
+				else:
+					print("Invalid Statement...")
+
 			# do next thing...the '=' will be the 2nd token so we can start at the 3rd token.
 			# the 3rd token will HAVE to start with an id and develop an expression... id op id op id ...blah
 		else:
 			print("This is an expression.")
+			while len(tokens) != 0:
+				if isExpression(tokens):
+					final.append(tokens.pop(0)) # id
+					final.append(tokens.pop(0)) # op
+					final.append(tokens.pop(0)) # id
+				else:
+					print("Invalid Statement...")
 			# do next thing... id op id (we already know first id is there, go to op)
 	else:
 		print("Error with first Id...")
@@ -42,6 +99,7 @@ for line in inputFile:
 	tokens = line.split()
 	checkAssignment(tokens)
 	# outputFile.write('Works\n')
+	print(final)
 
 
 inputFile.close()
